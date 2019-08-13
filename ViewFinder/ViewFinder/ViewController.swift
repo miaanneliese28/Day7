a2f4998c5a2dd361c69de2f384d3d4ab31e57f6a
Mia <mia@Apples-MacBook-Pro-30.local>
2019-08-13 10:02:53 -0400

    viewfinder

---
+++ ViewFinder/ViewFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ViewFinder
+//
+//  Created by Mia on 8/13/19.
+//  Copyright © 2019 Mia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
